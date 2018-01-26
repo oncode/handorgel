8d8c077acedb224edda8fe6c63a130007a540a97
Manuel Sommerhalder <manuel.sommerhalder@goldinteractive.ch>
2018-01-26 19:21:18 +0100

    Update rollup and postcss cli

--- .config/postcss.config.js
+++ .config/postcss.config.js
@@ -1,30 +1,25 @@
-var plugins = [
-  'autoprefixer',
-  'postcss-import',
-  'postcss-pxtorem',
-  'postcss-assets',
-  'css-mqpacker'
-]
-
-if (process.env.MODE === 'minified') {
-  plugins.push('cssnano')
-}
-
-module.exports = {
-  'use': plugins,
-  'cssnano': {
-    zindex: false
-  },
-  'postcss-pxtorem': {
-    rootValue: 16,
-    unitPrecision: 5,
-    propWhiteList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-    replace: true,
-    mediaQuery: false,
-    minPixelValue: 2
-  },
-  'postcss-assets': {
-    relative: true,
-    cachebuster: true
+module.exports = (ctx) => {
+  return {
+    map: ctx.options.map,
+    plugins: {
+      'autoprefixer': {},
+      'postcss-import': {},
+      'postcss-pxtorem': {
+        rootValue: 16,
+        unitPrecision: 5,
+        propWhiteList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+        replace: true,
+        mediaQuery: false,
+        minPixelValue: 2
+      },
+      'postcss-assets': {
+        relative: true,
+        cachebuster: true
+      },
+      'css-mqpacker': {},
+      'cssnano': ctx.env !== 'minified' ? false : {
+        zindex: false
+      }
+    }
   }
 }
